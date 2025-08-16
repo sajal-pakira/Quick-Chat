cf6c24a7c1d72a0fefd20f4b4a9a3b9baf566d89
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-16 20:14:55 +0530

    signup controller logic added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,31 @@
-export const signup = async (req, res) => {};
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { email, fullName, password, profilePic } = req.body;
+  try {
+    if (password.length < 6) {
+      return req.status(400).json({
+        message: "Password must be at least 6 characters",
+        success: false,
+      });
+    }
+    const user = await User.find({ email });
+    if (user) {
+      return req.status(400).json({
+        message: "Email already exists, Please Login",
+        success: false,
+      });
+    }
+    //hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      email,
+      fullName,
+      password: hashedPassword,
+    });
+  } catch (error) {}
+};
 export const login = async (req, res) => {};
 export const logout = async (req, res) => {};
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -16,10 +16,11 @@ const userSchema = new mongoose.Schema(
       required: [true, "Password is required"],
       minlength: [6, "Minumun password length should be 6"],
     },
-    profilePic:{
-        type:String,
-        default:"https://imgs.search.brave.com/mDztPWayQWWrIPAy2Hm_FNfDjDVgayj73RTnUIZ15L0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzE1Lzg0LzQz/LzM2MF9GXzIxNTg0/NDMyNV90dFg5WWlJ/SXllYVI3TmU2RWFM/TGpNQW15NEd2UEM2/OS5qcGc"
-    }
+    profilePic: {
+      type: String,
+      default:
+        "https://imgs.search.brave.com/mDztPWayQWWrIPAy2Hm_FNfDjDVgayj73RTnUIZ15L0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzE1Lzg0LzQz/LzM2MF9GXzIxNTg0/NDMyNV90dFg5WWlJ/SXllYVI3TmU2RWFM/TGpNQW15NEd2UEM2/OS5qcGc",
+    },
   },
   { timestamps: true }
 );
