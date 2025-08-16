49d44a7152887cce07653f86f9ca278035b68384
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-16 13:29:11 +0530

    authRouter exported & then imported to index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import authRouter from "./routes/auth.route";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 5001;
 
