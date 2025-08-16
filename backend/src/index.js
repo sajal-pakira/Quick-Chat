2d3679ac95202d1bfc4d4f1ea06729b895479e0f
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-16 18:16:54 +0530

    connectDB import

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRouter from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -10,4 +11,5 @@ const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
