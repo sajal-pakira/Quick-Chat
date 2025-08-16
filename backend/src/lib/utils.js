22aa842d06dbe609fedffd005e0d199aa35c7755
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-16 20:33:21 +0530

    JWT TOKEN GENERATION funCtion ADDED

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign(userId, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
