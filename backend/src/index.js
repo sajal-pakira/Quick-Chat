a098d61bb154e1f9c655169ea9fd825be36a9fe1
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-17 21:55:14 +0530

    userRoute imported into index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,14 @@ import dotenv from "dotenv";
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use("/api/user");
+app.use("/api/user", userRoute);
 
 const PORT = process.env.PORT || 5001;
 
