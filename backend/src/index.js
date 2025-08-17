39e9217023d8bc6a8f7f5507281d3fde99063ed3
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-17 22:36:43 +0530

    cookie-parser imported & implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import authRouter from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 import userRoute from "./routes/user.route.js";
@@ -8,6 +10,8 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRoute);
 
