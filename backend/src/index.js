b26dfb8de258fe71da7006c97f26f632f6684d17
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-17 21:43:42 +0530

    modification in configuration of dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
 import authRouter from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use("/api/user",)
+app.use("/api/user");
 
 const PORT = process.env.PORT || 5001;
 
