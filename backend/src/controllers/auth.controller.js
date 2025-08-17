8e4a59d30486af1dd98b433c796904c33596caca
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-17 21:22:35 +0530

    proper error handling in logout controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -101,14 +101,17 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   try {
     res.cookie("jwt", "", {
-    maxAge: 0,
-  });
-  res.status(200).json({
-    success: false,
-    message: "Logged out successfully",
-  });
+      maxAge: 0,
+    });
+    res.status(200).json({
+      success: false,
+      message: "Logged out successfully",
+    });
   } catch (error) {
-    
+    console.log("Error in logout controller :- ", error.message);
+    res.status(500).json({
+      success: false,
+      message: `${error.message}`,
+    });
   }
-  
 };
