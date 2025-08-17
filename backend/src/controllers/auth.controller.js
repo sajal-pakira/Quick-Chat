cf3208ab54f778adcb88a110d4a085ae65ca1fc3
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-17 21:19:41 +0530

    logout functionality implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -98,4 +98,17 @@ export const login = async (req, res) => {
     });
   }
 };
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", {
+    maxAge: 0,
+  });
+  res.status(200).json({
+    success: false,
+    message: "Logged out successfully",
+  });
+  } catch (error) {
+    
+  }
+  
+};
