fbb337014209b14040add2aad0b90be5e1a138c1
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-17 21:01:14 +0530

    after loggin in send user details as response

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -80,6 +80,16 @@ export const login = async (req, res) => {
       });
     }
     generateToken(user._id, res);
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      user: {
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName,
+        profilePic: user.profilePic,
+      },
+    });
   } catch (error) {}
 };
 export const logout = async (req, res) => {};
