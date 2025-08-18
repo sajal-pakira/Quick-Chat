b1f36b220906adec0877a129ce7c201b10ecce50
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 12:52:02 +0530

    after updating user profile , send updated user as response

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,16 +1,24 @@
 import cloudinary from "../lib/cloudinary.js";
 
 export const updateProfile = async (req, res) => {
-  const { fullName, profilePic } = req.body;
-  if (!fullName && !profilePic) {
-    return res.status(400).json({
-      message: "Any one field is required to update profile",
-      success: false,
-    });
-  }
-  if (fullName) req.user.fullName = fullName;
+  try {
+    const { fullName, profilePic } = req.body;
+    if (!fullName && !profilePic) {
+      return res.status(400).json({
+        message: "Any one field is required to update profile",
+        success: false,
+      });
+    }
+    if (fullName) req.user.fullName = fullName;
 
-  if (profilePic) {
-    const uploadResponse = cloudinary.uploader.upload(profilePic);
-  }
+    if (profilePic) {
+      const uploadResponse = cloudinary.uploader.upload(profilePic);
+      req.user.profilePic = (await uploadResponse).secure_url;
+    }
+
+    res.status(200).json({
+      message: "Profile updated successfully",
+      updatedUser: req.user,
+    });
+  } catch (error) {}
 };
