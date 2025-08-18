08d188cf462c1b07a892783b5cab1c68381efa73
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 18:33:17 +0530

    protectedRoute imported & used

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { protectedRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+
+router.get("/users",protectedRoute)
 
 export default router;
