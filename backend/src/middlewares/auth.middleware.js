7de377e08d89c30ef3e2f1361c6e6d0615cb4056
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 11:41:43 +0530

    error handling for invalid token

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -6,8 +6,15 @@ export const protectedRoute = async (req, res, next) => {
     if (!token) {
       return res.status(401).json({
         message: "Unauthorized - no token provided",
+        success: false,
       });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({
+        message: "Unauthorized - Invalid token",
+        success: false,
+      });
+    }
   } catch (error) {}
 };
