334c7329533b56b811825995e4c0aa95049bb874
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 18:05:04 +0530

    checkAuth file imported

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,17 @@
 import express from "express";
 
-import { login, logout, signup } from "../controllers/auth.controller.js";
-
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.get("/check", checkAuth);
 
 export default router;
