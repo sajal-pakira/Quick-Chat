d0abd461ec06381136d82e487f2b07e03f694adf
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 11:39:05 +0530

    error handling for unauthorized access

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,6 +1,12 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-
-export const protectedRoute= async (req,res,next) => {
-    
-}
+export const protectedRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized - no token provided"
+        })
+    }
+  } catch (error) {}
+};
