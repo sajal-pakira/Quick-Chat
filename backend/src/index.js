0d71711b82bdf6fd04ac07768d1698908f8de75f
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 18:29:50 +0530

    messageRoute created & imported & implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,10 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 
-import authRouter from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import authRouter from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
 
 const PORT = process.env.PORT || 5001;
 
