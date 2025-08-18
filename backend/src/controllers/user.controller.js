05f401fe3e699e212d6cb0a1f1151147b734d515
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 12:34:49 +0530

    destructuring fullName & profilePic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,9 @@
-export const updateProfile= () => {
-    
-}
+export const updateProfile = async (req, res) => {
+  const { fullName, profilePic } = req.body;
+  if (!fullName && !profilePic) {
+    return res.status(400).json({
+      message: "Any one field is required to update profile",
+      success: false,
+    });
+  }
+};
