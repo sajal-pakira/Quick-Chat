c40c3584ffbb7d22499797b468eb14ecab5b95d5
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 11:40:10 +0530

    jwt verified

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,10 +3,11 @@ import jwt from "jsonwebtoken";
 export const protectedRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if(!token){
-        return res.status(401).json({
-            message:"Unauthorized - no token provided"
-        })
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized - no token provided",
+      });
     }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (error) {}
 };
