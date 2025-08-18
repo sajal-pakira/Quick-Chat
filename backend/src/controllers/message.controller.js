b3189acfc05410fab766b44c20605ec9b3d02c30
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 18:54:56 +0530

    finding all the users except current user

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,8 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSideBar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
+    const filteredUser = await User.find({ _id: { $ne: loggedInUserId } });
   } catch (error) {}
 };
