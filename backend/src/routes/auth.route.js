e33035db713d8e06cbff8883bda1d32d211f5d00
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-18 18:06:29 +0530

    protectedRoute imported

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,12 +6,14 @@ import {
   logout,
   signup,
 } from "../controllers/auth.controller.js";
+import { protectedRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/check", checkAuth);
+
+router.get("/check", protectedRoute, checkAuth);
 
 export default router;
