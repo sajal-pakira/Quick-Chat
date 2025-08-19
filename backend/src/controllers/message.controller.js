979f13d5bd97bd54e1ccf03d07b76c91f7a1e112
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-19 17:59:48 +0530

    sendMessage created & imported to message route

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -37,3 +37,7 @@ export const getMessage = async (req, res) => {
     });
   }
 };
+
+export const sendMessage=async (req,res) => {
+  
+}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectedRoute } from "../middlewares/auth.middleware.js";
-import { getMessage, getUsersForSideBar } from "../controllers/message.controller.js";
+import { getMessage, getUsersForSideBar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectedRoute, getUsersForSideBar);
 router.get("/:id", protectedRoute, getMessage);
+router.get("/send/:id", protectedRoute, sendMessage);
 
 export default router;
