fb0ce45cd7b9094a9d00b525971ad65d1b396197
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-23 20:00:09 +0530

    HomePage created & added to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div className="">
       <Navbar />
+
+    <Routes>
+      <Route element={<HomePage/>}/>
+    </Routes>
+
     </div>
   );
 };
