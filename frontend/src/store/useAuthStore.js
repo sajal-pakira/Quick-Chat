20b34aaa941318e6966622371979d825fc2bcaf3
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-23 20:44:46 +0530

    modification in useAuthStore hook

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,5 +2,8 @@ import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
+  isLoggingIn: false,
+  isSigningUp: false,
+  isUpdatingProfile: false,
   isCheckingAuth: true,
 }));
