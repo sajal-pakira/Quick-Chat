4b4520bcd1e89e13626c76eda4d25909c6b00512
Sajal Pakira <uniquesajal6@gmail.com>
2025-08-23 20:18:05 +0530

    SettingsPage, SignUpPage, LoginPage, ProfilePage created & imported to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import SettingsPage from "./pages/SettingsPage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 const App = () => {
   return (
     <div className="">
       <Navbar />
 
-    <Routes>
-      <Route element={<HomePage/>}/>
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };
